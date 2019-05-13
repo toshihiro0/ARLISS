f526e8c2512c8ab2ed2ffc14495874f7bb6b409d
morutole <50069930+morutole@users.noreply.github.com>
2019-05-13 09:08:14 +0900

    GPS

---
+++ arduino/GPS/GPS.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3,4); //RX,TX
+
+void setup()
+{
+    Serial.begin(57600);
+    while(!Serial){}
+    mySerial.begin(9600);
+}
+
+void loop()
+{
+    if(mySerial.available()){
+        Serial.write(mySerial.read());
+    }
+}
