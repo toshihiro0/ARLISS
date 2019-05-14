ea69c94086caf454c12493e0a6c17ad625ffe7c0
morutole <50069930+morutole@users.noreply.github.com>
2019-05-14 23:58:59 +0900

    BME280の気圧と気温から高度計算

---
+++ Case/気圧センサ/BME280_height_calculate/BME280_height_calculate.ino
@@ -0,0 +1,39 @@
+#include <SparkFunBME280.h>
+
+const int SPI_CS_PIN = 10;
+BME280 sensor;
+
+static const float pressure_on_the_ground = 101540.265; //高度計算用の地上の気圧(Pa)
+static const float temperature_on_the_ground = 23.82; //高度計算用の地上の気温(℃)
+static const float temperature_correction = 273.15;
+
+void setup()
+{
+    Serial.begin(115200);
+    sensor.beginSPI(SPI_CS_PIN);
+}
+
+float temepratureK;
+float pressure;
+float height;
+
+void loop()
+{
+    Serial.print("Temp: ");
+    Serial.print(sensor.readTempC());
+
+    Serial.print(" °C, Humidity: ");
+    Serial.print(sensor.readFloatHumidity());
+
+    Serial.print(" %, Pressure: ");
+    Serial.print(sensor.readFloatPressure());
+    Serial.println(" Pa");
+    
+    pressure = sensor.readFloatPressure();
+    height = (1-pow(pressure/pressure_on_the_ground,0.19035714))/0.0065*(temperature_on_the_ground+temperature_correction);
+    Serial.print("Height: ");
+    Serial.print(height);
+    Serial.println(" m");
+
+    delay(1000);
+}
