0f41b2452e5515ba69b942cd6a49d909d5d14b32
morutole <50069930+morutole@users.noreply.github.com>
2019-06-03 19:32:41 +0900

    Update LoRaTest.ino

--- LoRa/LoRaTest/LoRaTest.ino
+++ LoRa/LoRaTest/LoRaTest.ino
@@ -3,6 +3,10 @@ SoftwareSerial LoRa_ss(2,3);
 
 void setup()
 {
+   pinMode(4,OUTPUT);
+   pinMode(5,OUTPUT);
+   digitalWrite(4,HIGH);
+   digitalWrite(5,HIGH);
    Serial.begin(57600);
    LoRa_ss.begin(19200);
 }
@@ -10,15 +14,19 @@ void setup()
 int number = 0;
 void loop() 
 {
-   if(LoRa_ss.available()){
-      Serial.write(LoRa_ss.read());
-   }
-   if(Serial.available()){
-      LoRa_ss.write(Serial.read());
-   }
-   /*LoRa_ss.print("hogehoge");
-   LoRa_ss.print(number);
-   LoRa_ss.print("\r");
-   ++number;
-   delay(1000);*/
+      if(LoRa_ss.available()){
+        Serial.write(LoRa_ss.read());
+      }
+      if(Serial.available()){
+        LoRa_ss.write(Serial.read());
+      }
+   
+   /*while(true){
+    LoRa_ss.print("hogehoge");
+    LoRa_ss.print(number);
+    LoRa_ss.print("\r");
+    ++number;
+    delay(1000);
+   }*/
+   
 }
