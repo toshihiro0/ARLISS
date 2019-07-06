00b27ee09e4afca928b7a013dd59d0a5c4abe456
morutole <50069930+morutole@users.noreply.github.com>
2019-07-06 19:59:01 +0900

    Update LoRa_Serial.ino

--- LoRa/LoRa_Serial/LoRa_Serial.ino
+++ LoRa/LoRa_Serial/LoRa_Serial.ino
@@ -1,5 +1,3 @@
-#include <SoftwareSerial.h>
-
 #define LoRa_sw 6
 #define LoRa_rst 7
 
@@ -10,7 +8,7 @@ void setup()
    pinMode(LoRa_rst,OUTPUT);
    digitalWrite(LoRa_rst,HIGH);
    Serial.begin(19200);
-   delay(2000);
+   delay(2000); //LoRa起動待ち
 }
 
 int number = 0;
