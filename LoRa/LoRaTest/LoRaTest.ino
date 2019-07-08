fc0176d8e5024048b329970616730522c550386c
morutole <50069930+morutole@users.noreply.github.com>
2019-07-08 17:48:30 +0900

    change for LoRa Test

--- LoRa/LoRaTest/LoRaTest.ino
+++ LoRa/LoRaTest/LoRaTest.ino
@@ -19,5 +19,4 @@ void loop()
    if(Serial.available()){
       LoRa_ss.write(Serial.read());//入力されたデータを送信
    }
-   delay(1000);
 }
--- LoRa/LoRa_string_judge/LoRa_string_judge.ino
+++ LoRa/LoRa_string_judge/LoRa_string_judge.ino
@@ -9,13 +9,9 @@ void setup()
     pinMode(7,OUTPUT);
     digitalWrite(6,HIGH);
     digitalWrite(7,HIGH);
-    pinMode(2,OUTPUT); //LEDの点滅でコード開始を確認
-    digitalWrite(2,HIGH);
-    delay(1000);
-    digitalWrite(2,LOW);
-
-    Serial.begin(57600); //Main serial port for console output
+    //Serial.begin(57600); //Main serial port for console output
     LoRa.begin(19200);
+    delay(2000);
 }
 
 void loop()
@@ -33,9 +29,6 @@ void loop()
 }
 void LoRa_recv(char *buf)
 {
-    char *string_pointer = buf;
-    int time1 = millis();
-    int time2;
     while (true) {
         while (LoRa.available() > 0) {
             *buf++ = LoRa.read();
@@ -45,13 +38,6 @@ void LoRa_recv(char *buf)
                 *buf = '\0';
                 return;
             }
-        }
-        time2 = millis();
-        if((time2-time1) > 10000){
-            digitalWrite(2,HIGH);
-            delay(2000);
-            digitalWrite(2,LOW);
-            time1 = time2;
         }
     }
 }
