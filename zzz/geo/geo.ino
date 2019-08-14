c0aec51741b0efae05d319843d20d941e26d1856
morutole <50069930+morutole@users.noreply.github.com>
2019-08-14 18:01:28 +0900

    temp test

---
+++ zzz/geo/geo.ino
@@ -0,0 +1,30 @@
+#include <math.h>
+
+#define goal_latitude 35.670357
+#define goal_longtitude 139.775163
+#define goal_altitude 5
+#define difference_lat 111316.2056
+static const float difference_lon = cos(goal_latitude/180*M_PI)*M_PI*6378.137/180*1000;
+
+void setup()
+{
+    Serial.begin(57600);
+}
+void loop()
+{
+    float lat = 35.672972;
+    float lon = 139.776284;
+    float alt = 5.0;
+    detect_waypoint(lat,lon,alt);
+}
+
+void detect_waypoint(float latitude,float longtitude,float altitude)
+{
+    float distance = sqrt((latitude-goal_latitude)*(latitude-goal_latitude)*difference_lat*difference_lat+(longtitude-goal_longtitude)*(longtitude-goal_longtitude)*difference_lon*difference_lon);
+    Serial.println(distance);
+    if((altitude-goal_altitude) < 20.0 && distance < 20.0){
+        return;
+    }else{
+        return;
+    }
+}
