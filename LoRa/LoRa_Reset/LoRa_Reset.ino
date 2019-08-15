e3c2bc990347a82f50c89d8122a8a5d16935d727
morutole <50069930+morutole@users.noreply.github.com>
2019-08-15 22:01:58 +0900

    test LoRa

--- LoRa/LoRa_Reset/LoRa_Reset.ino
+++ LoRa/LoRa_Reset/LoRa_Reset.ino
@@ -1,5 +1,5 @@
 #include<SoftwareSerial.h>
-SoftwareSerial LoRa_ss(8,9);
+SoftwareSerial LoRa_ss(17,19);
 
 void setup()
 {
@@ -7,8 +7,8 @@ void setup()
 
     pinMode(6,OUTPUT);
     digitalWrite(6,HIGH);
-    pinMode(7,OUTPUT);
-    digitalWrite(7,HIGH);
+    pinMode(18,OUTPUT);
+    digitalWrite(18,HIGH);
     
    	Serial.begin(57600);
    	LoRa_ss.begin(19200);
@@ -24,8 +24,8 @@ void loop()
     }
 	if(digitalRead(5) == LOW){
     while(digitalRead(5) == LOW){}
-		digitalWrite(7,LOW);
+		digitalWrite(18,LOW);
 		delay(1);
-    digitalWrite(7,HIGH);
+    digitalWrite(18,HIGH);
 	}
 }
---
+++ LoRa/LoRa_measure_and_cut_off/LoRa_measure_and_cut_off.ino
@@ -0,0 +1,109 @@
+#include <SoftwareSerial.h>
+#include <SparkFunBME280.h>
+#include <EEPROM.h> //EEPROMに保存
+
+#define nichrome_pin_1 2
+#define nichrome_pin_2 3
+#define SPI_CS_PIN 10 //気圧センサ
+
+#define LoRa_sw 6 //LoRaの電源ピン
+#define LoRa_rst 7 //LoRaのRstピン
+
+#define airpressure_on_the_ground 100853.04 //高度計算用の地上の気圧(Pa)
+static const float temperature_on_the_ground = 34.91; //高度計算用の地上の気温(℃)
+static const float temperature_correction = 273.15;
+
+SoftwareSerial LoRa(8,9);
+
+BME280 air_pressure_sensor; //BME280
+
+void setup()
+{
+    pinMode(nichrome_pin_1, OUTPUT);
+    pinMode(nichrome_pin_2, OUTPUT);
+    digitalWrite(nichrome_pin_1, LOW);
+    digitalWrite(nichrome_pin_2, LOW);
+
+    pinMode(LoRa_sw, OUTPUT);
+    pinMode(LoRa_rst,OUTPUT);
+    digitalWrite(LoRa_sw,HIGH);
+    digitalWrite(LoRa_rst,HIGH);
+
+    air_pressure_sensor.beginSPI(SPI_CS_PIN); //気圧センサとのSPI通信
+    LoRa.begin(19200); //LoRaとの通信
+    delay(2000); //LoRaの起動待ち
+}
+
+void loop()
+{
+    int i;
+    float height;
+    while(true){
+        char buf[128];
+        LoRa_recv(buf);
+        LoRa.print("return\r");
+        delay(400);
+        if(strstr(buf,"measure")!= NULL){
+            height = height_judge();
+            for(i = 0;i < 4;++i){
+                EEPROM.write(i,0); //putのために中身をClearしておく
+            }
+            EEPROM.put(0,height); //EEPROMに保存
+            LoRa.print(height);delay(100);
+            LoRa.print("\r");delay(100);
+        }else if(strstr(buf,"cutoff1")!= NULL){
+            LoRa.print("Yes,sir\r");
+            nichromcut1();
+            delay(100);
+        }else if(strstr(buf,"cutoff2")!= NULL){
+            LoRa.print("Yes,sir\r");
+            nichromcut2();
+            delay(100);
+        }
+    }
+}
+
+float height_judge()
+{
+    float pressure;
+    float height;
+    int i;
+    for(i = 0;i < 5;++i){ //値の取り始めは値がおかしい。
+        air_pressure_sensor.readTempC(); //よくわからないがこれを無くすと気圧の値がおかしくなる?????。
+        pressure = air_pressure_sensor.readFloatPressure();
+        delay(100);
+    }
+    air_pressure_sensor.readTempC(); //よくわからないがこれを無くすと気圧の値がおかしくなる?????。
+    pressure = air_pressure_sensor.readFloatPressure();
+    height = (1-pow(pressure/airpressure_on_the_ground,0.19035714))/0.0065*(temperature_on_the_ground+temperature_correction);
+    return height;
+}
+
+void LoRa_recv(char *buf)
+{
+    while (true) {
+        while (LoRa.available() > 0) {
+            *buf++ = LoRa.read();
+            if(*(buf-3) == 'O' && *(buf-2) == 'K' && *(buf-1) == '\r'){
+                continue;
+            }else if (*(buf-1) == '\r'){
+                *buf = '\0';
+                return;
+            }
+        }
+    }
+}
+
+void nichromcut1()
+{
+    digitalWrite(nichrome_pin_1,HIGH);
+    delay(6000);
+    digitalWrite(nichrome_pin_1,LOW);
+}
+
+void nichromcut2()
+{
+    digitalWrite(nichrome_pin_2,HIGH);
+    delay(6000);
+    digitalWrite(nichrome_pin_2,LOW);
+}
