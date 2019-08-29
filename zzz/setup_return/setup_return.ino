084c41258fc05547e9cc85d3bb69cd68653bce70
morutole <50069930+morutole@users.noreply.github.com>
2019-08-29 20:04:10 +0900

    Create setup_return.ino

---
+++ zzz/setup_return/setup_return.ino
@@ -0,0 +1,19 @@
+#include <EEPROM.h>
+
+void setup()
+{
+    Serial.begin(57600);
+    EEPROM.write(0,1);
+    if(EEPROM.read(0) == 1){
+        return;
+    }else{
+        delay(10000);
+    }
+}
+
+void loop()
+{
+    Serial.write("hogehoge");
+    delay(500);
+
+}
